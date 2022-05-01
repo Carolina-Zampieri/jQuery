adaba1fe5d460b9f73dc1faf7e078e3b260029a3
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2022-05-01 20:18:05 -0300

    Adicionando eventos

--- public/js/main.js
+++ public/js/main.js
@@ -62,8 +62,6 @@ function inicializaMarcadores(){
     campo.on("input", function(){
     var digitado = campo.val();
     var comparavel = frase.substr(0, digitado.length);
-        console.log("Digitado:" + digitado);
-        console.log("Frase C.:" + comparavel);
     if(digitado == comparavel){
         campo.addClass("borda-verde");
         campo.removeClass("borda-vermelha");
@@ -78,21 +76,35 @@ function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Carolina";
     var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
 
-    var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                    "<td>"+ botaoRemover + "</td>"+
-                "<tr>"
+    var linha = novaLinha(usuario, numPalavras);
 
     corpoTabela.prepend(linha);
 }
 
-$(".botao-remover").click(function(event){
+function novaLinha(usuario, palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(){
     event.preventDefault();
     $(this).parent().parent().remove();
-})
+}
 
 function reiniciaJogo(){
     campo.attr("disabled", false);
