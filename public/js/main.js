e271c63a4c9fd240fe4e1b5c0bc3284279c8e3f4
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2022-05-01 11:32:20 -0300

    Aprendendo a usar contadores no jQuery

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;  // /\S+/ busca por qualquer espaço vazio
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+});
+
+
+
+
+
+
+
