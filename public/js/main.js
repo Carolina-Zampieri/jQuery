28543ea1abca4d310bfee3a26afbc4b8ab50efdc
Carolina Zampieri Rodrigues <carolina.zampieri89@gmail.com>
2022-05-01 22:21:32 -0300

    Organizando o código

--- public/js/main.js
+++ public/js/main.js
@@ -72,40 +72,6 @@ function inicializaMarcadores(){
 })
 }
 
-function inserePlacar(){
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Carolina";
-    var numPalavras = $("#contador-palavras").text();
-
-    var linha = novaLinha(usuario, numPalavras);
-
-    corpoTabela.prepend(linha);
-}
-
-function novaLinha(usuario, palavras){
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-    var link = $("<a>").addClass("botao-remover").attr("href", "#");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone);
-
-    colunaRemover.append(link);
-
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-function removeLinha(){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
-
 function reiniciaJogo(){
     campo.attr("disabled", false);
     campo.val("");
